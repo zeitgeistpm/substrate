e8dcfc106299783db10f2eacf776eb4dcc13b93e
paritytech-ci <devops-team@parity.io>
2022-10-23 04:28:56 +0000

    ___Updated docs for master___

--- master/implementors/sp_runtime/traits/trait.Extrinsic.js
+++ master/implementors/sp_runtime/traits/trait.Extrinsic.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
 "sp_runtime":[],
-"sp_test_primitives":[["impl Extrinsic for <a class=\"enum\" href=\"sp_test_primitives/enum.Extrinsic.html\" title=\"enum sp_test_primitives::Extrinsic\">Extrinsic</a>"]],
+"sp_test_primitives":[["impl <a class=\"trait\" href=\"sp_runtime/traits/trait.Extrinsic.html\" title=\"trait sp_runtime::traits::Extrinsic\">Extrinsic</a> for <a class=\"enum\" href=\"sp_test_primitives/enum.Extrinsic.html\" title=\"enum sp_test_primitives::Extrinsic\">Extrinsic</a>"]],
 "substrate_test_runtime":[["impl <a class=\"trait\" href=\"sp_runtime/traits/trait.Extrinsic.html\" title=\"trait sp_runtime::traits::Extrinsic\">Extrinsic</a> for <a class=\"enum\" href=\"substrate_test_runtime/enum.Extrinsic.html\" title=\"enum substrate_test_runtime::Extrinsic\">Extrinsic</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
