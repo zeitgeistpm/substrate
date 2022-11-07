887c6df3d4c3446bd57332fb50ef3c3edbb888a2
paritytech-ci <devops-team@parity.io>
2022-11-07 22:13:25 +0000

    ___Updated docs for master___

--- master/implementors/sp_application_crypto/traits/trait.AppSignature.js
+++ master/implementors/sp_application_crypto/traits/trait.AppSignature.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
-"beefy_primitives":[["impl AppSignature for <a class=\"struct\" href=\"beefy_primitives/crypto/struct.Signature.html\" title=\"struct beefy_primitives::crypto::Signature\">Signature</a>"]],
+"beefy_primitives":[["impl <a class=\"trait\" href=\"sp_application_crypto/traits/trait.AppSignature.html\" title=\"trait sp_application_crypto::traits::AppSignature\">AppSignature</a> for <a class=\"struct\" href=\"beefy_primitives/crypto/struct.Signature.html\" title=\"struct beefy_primitives::crypto::Signature\">Signature</a>"]],
 "pallet_example_offchain_worker":[["impl <a class=\"trait\" href=\"sp_application_crypto/traits/trait.AppSignature.html\" title=\"trait sp_application_crypto::traits::AppSignature\">AppSignature</a> for <a class=\"struct\" href=\"pallet_example_offchain_worker/crypto/struct.Signature.html\" title=\"struct pallet_example_offchain_worker::crypto::Signature\">Signature</a>"]],
 "sp_application_crypto":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
