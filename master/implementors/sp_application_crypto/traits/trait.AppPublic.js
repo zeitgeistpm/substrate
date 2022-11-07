fb3a835e2a899de5edd41bffdff225de309bd5e8
paritytech-ci <devops-team@parity.io>
2022-11-07 20:42:35 +0000

    ___Updated docs for master___

--- master/implementors/sp_application_crypto/traits/trait.AppPublic.js
+++ master/implementors/sp_application_crypto/traits/trait.AppPublic.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
-"beefy_primitives":[["impl <a class=\"trait\" href=\"sp_application_crypto/traits/trait.AppPublic.html\" title=\"trait sp_application_crypto::traits::AppPublic\">AppPublic</a> for <a class=\"struct\" href=\"beefy_primitives/crypto/struct.Public.html\" title=\"struct beefy_primitives::crypto::Public\">Public</a>"]],
+"beefy_primitives":[["impl AppPublic for <a class=\"struct\" href=\"beefy_primitives/crypto/struct.Public.html\" title=\"struct beefy_primitives::crypto::Public\">Public</a>"]],
 "pallet_example_offchain_worker":[["impl <a class=\"trait\" href=\"sp_application_crypto/traits/trait.AppPublic.html\" title=\"trait sp_application_crypto::traits::AppPublic\">AppPublic</a> for <a class=\"struct\" href=\"pallet_example_offchain_worker/crypto/struct.Public.html\" title=\"struct pallet_example_offchain_worker::crypto::Public\">Public</a>"]],
 "sp_application_crypto":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
