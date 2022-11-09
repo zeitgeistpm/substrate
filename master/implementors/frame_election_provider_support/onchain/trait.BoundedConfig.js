a6824703902bb135e008c2a28397e59d0d905e48
paritytech-ci <devops-team@parity.io>
2022-11-09 09:44:49 +0000

    ___Updated docs for master___

--- master/implementors/frame_election_provider_support/onchain/trait.BoundedConfig.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"kitchensink_runtime":[["impl <a class=\"trait\" href=\"frame_election_provider_support/onchain/trait.BoundedConfig.html\" title=\"trait frame_election_provider_support::onchain::BoundedConfig\">BoundedConfig</a> for <a class=\"struct\" href=\"kitchensink_runtime/struct.OnChainSeqPhragmen.html\" title=\"struct kitchensink_runtime::OnChainSeqPhragmen\">OnChainSeqPhragmen</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
