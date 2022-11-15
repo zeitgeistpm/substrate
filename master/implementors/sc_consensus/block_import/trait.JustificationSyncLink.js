99b9e05cdb3ed61f1473981fa0f8cc6144424f33
paritytech-ci <devops-team@parity.io>
2022-11-15 09:48:42 +0000

    ___Updated docs for master___

--- master/implementors/sc_consensus/block_import/trait.JustificationSyncLink.js
+++ master/implementors/sc_consensus/block_import/trait.JustificationSyncLink.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "sc_consensus":[],
-"sc_network":[["impl&lt;B:&nbsp;BlockT, H:&nbsp;<a class=\"trait\" href=\"sc_network/config/trait.ExHashT.html\" title=\"trait sc_network::config::ExHashT\">ExHashT</a>&gt; JustificationSyncLink&lt;B&gt; for <a class=\"struct\" href=\"sc_network/struct.NetworkService.html\" title=\"struct sc_network::NetworkService\">NetworkService</a>&lt;B, H&gt;"]]
+"sc_network":[["impl&lt;B:&nbsp;<a class=\"trait\" href=\"sp_runtime/traits/trait.Block.html\" title=\"trait sp_runtime::traits::Block\">BlockT</a>, H:&nbsp;<a class=\"trait\" href=\"sc_network/config/trait.ExHashT.html\" title=\"trait sc_network::config::ExHashT\">ExHashT</a>&gt; JustificationSyncLink&lt;B&gt; for <a class=\"struct\" href=\"sc_network/struct.NetworkService.html\" title=\"struct sc_network::NetworkService\">NetworkService</a>&lt;B, H&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- master/implementors/sp_inherents/client_side/trait.InherentDataProvider.js
+++ master/implementors/sp_inherents/client_side/trait.InherentDataProvider.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {
 "sc_consensus_manual_seal":[["impl InherentDataProvider for <a class=\"struct\" href=\"sc_consensus_manual_seal/consensus/timestamp/struct.SlotTimestampProvider.html\" title=\"struct sc_consensus_manual_seal::consensus::timestamp::SlotTimestampProvider\">SlotTimestampProvider</a>"]],
-"sp_authorship":[["impl&lt;H:&nbsp;HeaderT&gt; InherentDataProvider for <a class=\"struct\" href=\"sp_authorship/struct.InherentDataProvider.html\" title=\"struct sp_authorship::InherentDataProvider\">InherentDataProvider</a>&lt;H&gt;"]],
+"sp_authorship":[["impl&lt;H:&nbsp;<a class=\"trait\" href=\"sp_runtime/traits/trait.Header.html\" title=\"trait sp_runtime::traits::Header\">HeaderT</a>&gt; InherentDataProvider for <a class=\"struct\" href=\"sp_authorship/struct.InherentDataProvider.html\" title=\"struct sp_authorship::InherentDataProvider\">InherentDataProvider</a>&lt;H&gt;"]],
 "sp_consensus_aura":[["impl InherentDataProvider for <a class=\"struct\" href=\"sp_consensus_aura/inherents/struct.InherentDataProvider.html\" title=\"struct sp_consensus_aura::inherents::InherentDataProvider\">InherentDataProvider</a>"]],
 "sp_consensus_babe":[["impl InherentDataProvider for <a class=\"struct\" href=\"sp_consensus_babe/inherents/struct.InherentDataProvider.html\" title=\"struct sp_consensus_babe::inherents::InherentDataProvider\">InherentDataProvider</a>"]],
 "sp_inherents":[],
